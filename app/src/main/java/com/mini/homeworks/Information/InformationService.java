614e54c5f61577d2d20dd1ca79b50ba375c9aaa6
Ljl233 <`2955441566@qq.com`>
2019-04-01 08:52:36 +0800

    25%的InformationActivity

---
+++ app/src/main/java/com/mini/homeworks/Information/InformationService.java
@@ -0,0 +1,11 @@
+package com.mini.homeworks.Information;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+public interface InformationService {
+    @GET("/api/userInfo/")
+    Call<InformationBean> getInformationBean(@Header("token") String token);
+
+}
